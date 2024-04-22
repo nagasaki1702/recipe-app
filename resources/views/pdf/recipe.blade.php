b9307f39cb6c3c552aa3dab6f9d518ecfa68eda8
nagasaki1702 <102396852+nagasaki1702@users.noreply.github.com>
2024-04-22 14:11:08 +0900

    hover時の色をグレーに変更

--- resources/views/pdf/recipe.blade.php
+++ resources/views/pdf/recipe.blade.php
@@ -5,45 +5,37 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Recipe PDF</title>
     <style>
-
         .recipe-info {
             text-align: center;
             margin-bottom: 20px;
         }
-
         .recipe-info img {
             max-width: 100%;
             height: auto;
         }
-
-
-    /* dompdf日本語文字化け対策 */
-    /* フォントをみんなipagに設定しておかないと日本語にならない・・ */
-    @font-face {
-        font-family: 'ipag';
-        font-style: normal;
-        font-weight: normal;
-        src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
-    }
-
-    @font-face {
-        font-family: 'ipag';
-        font-style: bold;
-        font-weight: bold;
-        src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
-    }
-
-    body {
-        font-family: 'ipag', 'Arial', sans-serif;
-    }
-
-    html, body, textarea {
-        font-family: 'ipag', 'Arial', sans-serif;
-    }
-
-
-</style>
+        /* dompdf日本語文字化け対策 */
+        /* フォントをみんなipagに設定しておかないと日本語にならない・・ */
+        @font-face {
+            font-family: 'ipag';
+            font-style: normal;
+            font-weight: normal;
+            src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
+        }
+        @font-face {
+            font-family: 'ipag';
+            font-style: bold;
+            font-weight: bold;
+            src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
+        }
+        body {
+            font-family: 'ipag', 'Arial', sans-serif;
+        }
+        html, body, textarea {
+            font-family: 'ipag', 'Arial', sans-serif;
+        }
+    </style>
 </head>
+
 <body>
     <div class="recipe-info">
         <h1>{{ $recipe->title }}</h1>
